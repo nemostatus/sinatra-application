720ce1b216830df03686063e07703f9c5c4eec0d
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-04-02 13:11:37 +0000

    creating post method in usercontroller class

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,11 @@ class UserController < ApplicationController
   get '/users/signup' do
     erb :"/users/signup"
   end
+  
+  post '/users/signup' do 
+ @user = User.create(email: params[:email], 
+ username: params[:username], 
+ password: params[:password]
+ )
+  end 
 end 
