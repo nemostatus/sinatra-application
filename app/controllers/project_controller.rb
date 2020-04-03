006806a7f5be68c78ace8c0ab1429e20200f0c28
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-04-03 08:55:03 +0000

    adding show page for a specific project

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -10,7 +10,13 @@ class ProjectController < ApplicationController
    deadline: params[:deadline],
    rating: params[:rating]= params[:comfort].to_i+params[:passion].to_i+params[:deadline].to_i
    )
-  binding.pry
+ redirect "/projects/#{@project.id}"
   end 
-
-end
+  
+  get '/projects/:id' do 
+    @project = Project.find(params[:id])
+    erb :'/projects/show'
+  end 
+    
+    
+  end
