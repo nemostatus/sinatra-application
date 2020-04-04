7fe9fe4093fcd4d18234ce7e1c7a3b1d2bf086eb
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-04-04 11:15:44 +0000

    adding session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     set :method_override, true
-
+    set :session_secret, "1s2a3l4t"
   end
 
   get "/" do
