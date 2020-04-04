d952e9afa3ec8e310f6e9142e190661b83943707
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-04-04 13:59:35 +0000

    trying to figure out user auth

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,52 +1,35 @@
-
+  
+require './app/controllers/project_controller.rb'
 class UserController < ApplicationController
   get '/users/signup' do
-      if logged_in? 
-         redirect "/users/#{current_user.id}"
-      else
     erb :"/users/signup"
-  end
   end
   
   post '/users/signup' do #need middleware for delete action #need to check in tux what my db looks like
     if params[:username] = "" && params[:password] == ""
       redirect "/users/signup"
-  else
-  @user = User.create(email: params[:email], 
+    else 
+       @user = User.create(email: params[:email], 
        username: params[:username], 
       password: params[:password]
        )
-    
+    session[:user_id] = @user.id
     redirect "/users/#{@user.id}"
  end   
  end 
  
   get '/users/login' do
-     if logged_in?   
-         redirect "/users/#{current_user.id}"  
-      else
-         erb :'/users/login'
-      end
-    end
- 
+  erb :'/users/login'
+  
+  end 
   
   post '/users/login' do 
     @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect "/users/#{@user.id}"
-    else 
-      redirect "/users/login"
   end 
-  end
   
   get '/users/:id' do
-    @user = User.find(params[:id])
+    @user = User.find_by(params[:id])
     erb :'/users/show'
-  end
-  
-  get '/logout' do 
-    session.clear 
-    redirect '/users/login'
-  end 
+   
+end
 end
