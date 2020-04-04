e3fc9183247c88697b44a75be05c12ca99813075
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-04-04 12:41:02 +0000

    adding user logout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,30 +5,43 @@ class UserController < ApplicationController
   end
   
   post '/users/signup' do #need middleware for delete action #need to check in tux what my db looks like
-    if params[:username] = "" || params[:password] == ""
+    if params[:username] = "" && params[:password] == ""
       redirect "/users/signup"
-    else 
-       @user = User.create(email: params[:email], 
+  else
+  @user = User.create(email: params[:email], 
        username: params[:username], 
       password: params[:password]
        )
-    session[:user_id] = @user.id
+    
     redirect "/users/#{@user.id}"
  end   
  end 
  
   get '/users/login' do
-  erb :'/users/login'
-  
-  end 
+     if logged_in?   
+         redirect "/users/#{current_user.id}"  
+      else
+         erb :'/users/login'
+      end
+ 
   
   post '/users/login' do 
     @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else 
+      redirect "/users/login"
   end 
+  end
   
   get '/users/:id' do
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
     erb :'/users/show'
-   
+  end
+  
+  get '/logout' do 
+    session.clear 
+    redirect '/users/login'
+  end 
 end
-end 
