a35da7b4d5ce101bb82ecd465cdc9899289d0228
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-04-10 08:49:04 +0000

    indent

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,17 +1,16 @@
   
 class UserController < ApplicationController
     get '/users/signup' do
-       if logged_in? 
-         redirect "/users/#{current_user.id}"
-      else
-         erb :'/users/signup'        
-      end 
+     if logged_in? 
+       redirect "/users/#{current_user.id}"
+     else
+       redirect '/users/signup'        
+    end 
     end
 
 
   post '/users/signup' do 
    if params[:username] == "" || params[:password] == ""
-    
      redirect '/users/signup' 
    else
     @user = User.new(email: params[:email], 
@@ -19,40 +18,34 @@ class UserController < ApplicationController
     password: params[:password]
     )
     @user.save
- 
     session[:user_id] = @user.id
-   
-
-   redirect "/users/#{@user.id}"
+    redirect "/users/#{@user.id}"
  end 
  end 
  
  get '/login' do 
-    if logged_in?   
-         redirect "/users/#{current_user.id}"  
-      else
-  erb :'/users/login'
+   if logged_in?   
+    redirect "/users/#{current_user.id}"  
+   else
+    erb :'/users/login'
  end
  end 
  
  post '/login' do 
-  
-    @user = User.find_by(username: params[:username])
+   @user = User.find_by(username: params[:username])
       if @user && @user.authenticate(params[:password])
          session[:user_id] = @user.id 
          redirect "/users/#{@user.id}"
       else  
          redirect "/login"
-      end 
-
-     end
+  end 
+  end
  
   get '/users/:id' do
     #conditionial needed to view the endpoint
    @user = User.find(params[:id]) 
-  
-     if @user.id == current_user.id #im testing this line if user.id doesnt match current_users id then it will go to auth
-     erb :'/users/show'
+    if @user.id == current_user.id #im testing this line if user.id doesnt match current_users id then it will go to auth
+      erb :'/users/show'
     else 
       erb :'/users/authentication'
   end
